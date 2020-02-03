6b03ab4de95ebb49da747bff4d764922c408c367
vetsb <dmitriylebyodkin@gmail.com>
2020-02-03 18:01:07 +0300

    Refactored binary search

--- src/binary_search/main.kt
+++ src/binary_search/main.kt
@@ -1,15 +1,33 @@
 package binary_search
 
-val source = arrayListOf<Int>().apply {
-    addAll(1..100_000_000)
+fun main() {
+  val num = 20
+  var array = Array(num) { it }
+      .toList()
+      .shuffled()
+      .toTypedArray()
+
+  array = quick_sort.quickSort(array)
+
+  println("Initial array: ${array.toList()}")
+
+  val index = binarySearch(array, 11)
+
+  println("Index of zero = $index")
 }
 
-fun main() {
-    val max = source.max()!!
+fun binarySearch(array: Array<Int>, number: Int): Int {
+  var firstIndex = 0
+  var lastIndex = array.size - 1
 
-    val binaryStart = System.currentTimeMillis()
-    val maxIndex = source.binarySearch(max)
-    val binaryEnd = System.currentTimeMillis()
+  while (firstIndex <= lastIndex) {
+    val middleIndex = (firstIndex + lastIndex) / 2
 
-    println("Binary search: $maxIndex. Duration: ${binaryEnd - binaryStart}")
+    when {
+      array[middleIndex] === number -> return middleIndex
+      array[middleIndex] < number -> firstIndex = middleIndex + 1
+      array[middleIndex] > number -> lastIndex = middleIndex - 1
+    }
+  }
+  return -1
 }
